1ebf4370f919dbfcf424fcaee4398b7516489bb6
MicheleIanni86 <micheleianni86@gmail.com>
2024-07-12 00:22:15 +0200

    feat: adjust header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
 <script>
+// Supports weights 100-800
+import '@fontsource-variable/martian-mono';
+
 
 </script>
 
 <template>
   <div class="header-container">
-    <div class="container">
-      <h3>Header!!</h3>
+    <div class="border-container">
+
+      <div class="container d-flex justify-content-center align-items-center gap-3">
+        <h1 class="m-0">MICHELE IANNI</h1>
+        <h2 class="m-0 mx-4"><font-awesome-icon icon="laptop-code" beat /></h2>
+        <h2 class="m-0">Jr. Full Stack Developer</h2>
+      </div>
     </div>
   </div>
 
@@ -13,8 +21,22 @@
 
 <style lang="scss" scoped>
 .header-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100px;
   background-color: #000;
   color: red;
+
+  .border-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 99%;
+    height: 89%;
+    border: 5px solid greenyellow;
+    color: greenyellow;
+    font-family: 'Martian Mono Variable', monospace;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue';
-
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.scss';
 
-createApp(App).mount('#app');
+// Import Font Awesome core
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Import Font Awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Import specific icons
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+
+// Add icons to the library
+library.add(faLaptopCode);
+
+// Create the app
+const app = createApp(App);
+
+// Register the Font Awesome component globally
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+// Mount the app
+app.mount('#app');
